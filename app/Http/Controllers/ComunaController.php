2577320b480b58c72e25fa8c428285c6c0c43855
miguelserr <aristizabalmiguelangel@gmail.com>
2024-10-03 23:54:45 -0500

    Commune Add

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -27,18 +27,33 @@ class ComunaController extends Controller
 
     /**
      * Show the form for creating a new resource.
+      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+        return view('comuna.new' , ['municipios' => $municipios]);
     }
 
+
     /**
+     * 
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio' , 'tb_comuna.muni_codi' , '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*' , "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index' , ['comunas' => $comunas]); 
     }
 
     /**
