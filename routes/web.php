2af67264df3fabc308945458646fac1f0c2a5a52
miguelserr <aristizabalmiguelangel@gmail.com>
2024-10-05 12:01:40 -0500

    Comuna edit

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@ Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index'
 Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
