9261c6fc545f840e601210cdf6b51d9763f07e0b
miguelserr <aristizabalmiguelangel@gmail.com>
2024-10-05 23:37:40 -0500

    muni list y add

---
+++ app/Http/Controllers/MunicipioController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Municipio;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MunicipioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //$municipios = Municipio::all();
+        $municipios = DB::table('tb_municipio')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+            ->get();
+        return view('municipio.index', ['municipios'=>$municipios]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $departamentos = DB::table('tb_departamento')
+        ->orderBy('depa_nomb')
+        ->get();
+        return view('municipio.new', ['departamentos' => $departamentos]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $municipio = new Municipio();
+        // $municipio->muni_codi = $request->id;
+        // El código de municipio es auto incremental
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+        ->get();
+        return view('municipio.index', ['municipios' => $municipios]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//RUTAS COMUNA
 
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
 Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
@@ -14,3 +16,10 @@ Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+
+//RUTAS MUNICIPIO
+
+
+Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios.index');
+Route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
+Route::get('/municipios/create', [MunicipioController::class, 'create'])->name('municipios.create');
