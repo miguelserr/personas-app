b7aa9d1e361c0d2f94fa6f99515647b7387ad30f
miguelserr <aristizabalmiguelangel@gmail.com>
2024-10-07 22:34:21 -0500

    list pais

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComunaController;
 use App\Http\Controllers\MunicipioController;
 use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\PaisController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,3 +37,7 @@ Route::get('/departamentos/create', [DepartamentoController::class, 'create'])->
 Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
 Route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update'])->name('departamentos.update');
 Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
+
+//RUTA PAIS
+
+Route::get('/pais', [PaisController::class, 'index']);
